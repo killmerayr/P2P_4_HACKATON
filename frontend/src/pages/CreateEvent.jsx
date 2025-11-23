e100189a0bdb646b863fcc31b027984d58e44e38
waivi <valentina250106@yandex.ru>
2025-11-23 13:51:36 +0700

    Update CreateEvent.jsx

--- frontend/src/pages/CreateEvent.jsx
+++ frontend/src/pages/CreateEvent.jsx
@@ -13,7 +13,6 @@ export default function CreateEvent() {
     contactInfo: ''
   });
 
-  // УБЕРИ лишнюю функцию - у тебя две handleSubmit
   // const handleSumbit = async (formData) => {
   //   try {
   //     await eventAPI.createEvent(formData);
