52d0b803dff8c396b5598d6f29a4bbe3aaec8fac
docRevi <andrejsavchenko91@gmail.com>
2025-11-23 09:50:06 +0700

    Fix

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
-<<<<<<< HEAD
-=======
-
->>>>>>> cfe9e6f43f0452ffa08c65e07119ec700ba26b77
 
 const api = axios.create({
   baseURL: API_BASE_URL,
